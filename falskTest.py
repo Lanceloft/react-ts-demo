3d2730b2d86ce0cbd0562e2a97b83a1ff08dc705
Lanceloft <lanceloft@LanceloftdeiMac.local>
2019-05-10 19:24:06 +0800

    add register

--- falskTest.py
+++ falskTest.py
@@ -4,6 +4,9 @@ from flask_restful import reqparse, abort, Api, Resource
 from bson.json_util import loads, dumps, RELAXED_JSON_OPTIONS, CANONICAL_JSON_OPTIONS
 import pymongo
 import json
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app, g
+import hashlib
 
 from CosPy import *
 
@@ -15,13 +18,16 @@ parser = reqparse.RequestParser()
 parser.add_argument('task')
 parser.add_argument('id')
 parser.add_argument('image')
-
+parser.add_argument('username')
+parser.add_argument('password')
 
 client = pymongo.MongoClient(host='localhost', port=27017)
 db = client.test
 collection = db.todos
 
 idCollection = db.students
+usersCollection = db.users
+
 
 class TestRoute(Resource):
     def get(self):
@@ -74,10 +80,35 @@ class EditRoute(Resource):
             }
             return res
 
+class RegisterRoute(Resource):
+    def post(self):
+        args = parser.parse_args()
+        username = args['username']
+        password = args['password']
+        if (usersCollection.find_one({'username': args['username']})) is None:
+            insertUser = usersCollection.insert_one({'username': username, 'password': hashlib.sha256(password.encode("utf-8")).hexdigest()})
+            if insertUser.inserted_ids:
+                res = {
+                  "status": 0,
+                }
+                return res
+            else:
+                res = {
+                  "status": 1,
+                }
+                return res
+        else:
+            res = {
+                "status": 1,
+                "message": '用户名重复'
+            }
+            return res
+
 api.add_resource(TestRoute, '/test')
 api.add_resource(DeleteRouter, '/test/delete')
 api.add_resource(EditRoute, '/test/edit')
 api.add_resource(GetCos, '/test/getImagesList')
+api.add_resource(RegisterRoute, '/test/register')
 
 
 if __name__ == '__main__':
--- src/pages/Test/Index.tsx
+++ src/pages/Test/Index.tsx
@@ -1,7 +1,111 @@
 import * as React from "react";
+import { Input, Button } from "antd";
+import http from "@/common/http.js";
+import "./Index.less";
 
-const TestPage: React.SFC = () => {
-  return <div>test page</div>;
-};
+export interface IHomePageState {
+  userName: string;
+  password: string;
+  loginUserName: string;
+  loginPassword: string;
+}
 
-export default TestPage;
+export interface IHomePageProps {}
+
+class LoginComponent extends React.Component<IHomePageProps, IHomePageState> {
+  constructor(props: IHomePageProps) {
+    super(props);
+    this.state = {
+      userName: "",
+      password: "",
+      loginUserName: "",
+      loginPassword: ""
+    };
+  }
+
+  public userNameOnChange = (e: any) => {
+    this.setState({
+      userName: e.target.value
+    });
+  };
+
+  public passwordOnChange = (e: any) => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  public loginUserNameOnChange = (e: any) => {
+    this.setState({
+      loginUserName: e.target.value
+    });
+  };
+
+  public loginPasswordOnChange = (e: any) => {
+    this.setState({
+      loginPassword: e.target.value
+    });
+  };
+
+  public register = () => {
+    let param = {
+      username: this.state.userName,
+      password: this.state.password
+    };
+
+    http.post("/test/register", param).then(data => {
+      console.log(data);
+    });
+  };
+
+  render() {
+    return (
+      <div className="login-form">
+        <div>注册:</div>
+        <div className="login-item">
+          <div className="item-name">用户名</div>
+          <Input
+            className="item-value"
+            placeholder="user name"
+            value={this.state.userName}
+            onChange={this.userNameOnChange}
+          />
+        </div>
+        <div className="login-item">
+          <div className="item-name">密码</div>
+          <Input
+            className="item-value"
+            placeholder="password"
+            value={this.state.password}
+            onChange={this.passwordOnChange}
+          />
+        </div>
+        <Button type="primary" onClick={this.register}>
+          注册
+        </Button>
+        <div>登录:</div>
+        <div className="login-item">
+          <div className="item-name">用户名</div>
+          <Input
+            className="item-value"
+            placeholder="user name"
+            value={this.state.loginUserName}
+            onChange={this.loginUserNameOnChange}
+          />
+        </div>
+        <div className="login-item">
+          <div className="item-name">密码</div>
+          <Input
+            className="item-value"
+            placeholder="password"
+            value={this.state.loginPassword}
+            onChange={this.loginPasswordOnChange}
+          />
+        </div>
+        <Button type="primary">登录</Button>
+      </div>
+    );
+  }
+}
+
+export default LoginComponent;
