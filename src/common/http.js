33c4f07c2056f062b4ca764da691d611f2a4eb4a
lanceloft <lanceloft@foxmail.com>
2019-10-10 17:02:16 +0800

    修改http host

--- src/common/http.js
+++ src/common/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { message } from "antd";
 import { getCookie } from "./cookies";
 
-const baseUrl = "http://127.0.0.1:5000";
+const baseUrl = "http://0.0.0.0:5000";
 
 const http = {
   get(repo, param) {
