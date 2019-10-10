1d42c80b13a46a1e7850ad2ef64baaea9eaf6865
lanceloft <lanceloft@foxmail.com>
2019-10-10 15:34:37 +0800

    add build

---
+++ build/precache-manifest.287c7786978c3833ef737570f36775fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "180478cab71f7f4eb046",
+    "url": "/static/js/main.e3f3fcbc.chunk.js"
+  },
+  {
+    "revision": "ed88de69095c810d5c6c",
+    "url": "/static/js/2.d1b8d543.chunk.js"
+  },
+  {
+    "revision": "180478cab71f7f4eb046",
+    "url": "/static/css/main.2f4bc728.chunk.css"
+  },
+  {
+    "revision": "ed88de69095c810d5c6c",
+    "url": "/static/css/2.ddd9b1f7.chunk.css"
+  },
+  {
+    "revision": "28fd3480420fded22cbe5f9bbf16d718",
+    "url": "/index.html"
+  }
+];
