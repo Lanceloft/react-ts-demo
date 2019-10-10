e7353d496f7b945c78370011cb21b192b86ed865
lanceloft <lanceloft@foxmail.com>
2019-10-10 16:53:02 +0800

    修改flaskhost

--- falskTest.py
+++ falskTest.py
@@ -204,4 +204,4 @@ api.add_resource(RegisterRoute, '/test/register')
 api.add_resource(LoginRouter, '/test/login')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug = True)
